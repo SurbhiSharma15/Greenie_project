33f913d5b5de3d647db54ee3832762aadfe86720
surbhi sharma <surbhisharmaias@gmail.com>
2023-11-18 23:16:40 +0530

    change data

--- server.js
+++ server.js
@@ -9,14 +9,17 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const userData = [
-    { id: 1, username: 'JohnDoe', email: 'john@example.com', phone: '123-456-7890', creationDate: '2022-01-01' },
-    { id: 2, username: 'JaneDoe', email: 'jane@example.com', phone: '987-654-3210', creationDate: '2022-02-01' },
-    { id: 2, username: 'surbhi', email: 'jane@example.com', phone: '987-654-3210', creationDate: '2022-02-01' },
-    { id: 2, username: 'kshitiz', email: 'jane@example.com', phone: '987-654-3210', creationDate: '2022-02-01' },
-    { id: 2, username: 'hehe', email: 'jane@example.com', phone: '987-654-3210', creationDate: '2022-02-01' },
-    { id: 2, username: 'huihuicutie', email: 'jane@example.com', phone: '987-654-3210', creationDate: '2022-02-01' },
-    { id: 2, username: 'geetu', email: 'jane@example.com', phone: '987-654-3210', creationDate: '2022-02-01' },
-    { id: 2, username: 'janie', email: 'jane@example.com', phone: '987-654-3210', creationDate: '2022-02-01' }
+    { id: 1, username: 'rohit', email: 'john@example.com', phone: '123-456-7890', creationDate: '2022-01-01' },
+    { id: 2, username: 'kohli', email: 'kohli@example.com', phone: '987-654-3210', creationDate: '2022-05-01' },
+    { id: 3, username: 'rahul', email: 'rahul@example.com', phone: '987-654-3210', creationDate: '2022-03-01' },
+    { id: 4, username: 'ishan', email: 'ishan@example.com', phone: '987-654-3210', creationDate: '2021-02-01' },
+    { id: 5, username: 'sky', email: 'sky@example.com', phone: '987-654-3210', creationDate: '2020-09-01' },
+    { id: 6, username: 'pant', email: 'pant@example.com', phone: '987-654-3210', creationDate: '2022-02-10' },
+    { id: 7, username: 'gill', email: 'gill@example.com', phone: '987-654-3210', creationDate: '2020-02-08' },
+    { id: 8, username: 'bumrah', email: 'bumrah@example.com', phone: '987-654-3210', creationDate: '2022-02-01' },
+    { id: 9, username: 'shami', email: 'shami@example.com', phone: '987-654-3210', creationDate: '2022-02-01' },
+    { id: 10, username: 'siraj', email: 'siraj@example.com', phone: '987-654-3210', creationDate: '2022-02-01' },
+    { id: 11, username: 'kuldeep', email: 'kuldeep@example.com', phone: '987-654-3210', creationDate: '2022-02-01' }
     // Add more mock users as needed
   ];
 
@@ -63,17 +66,3 @@ const userData = [
 
 
 
-// app.get('/api/users', (req, res) => {
-//   // Simulate a placeholder database with mock user data
-//   const userData = [
-//     { id: 1, username: 'JohnDoe', email: 'john@example.com', phone: '123-456-7890', creationDate: '2022-01-01' },
-//     { id: 2, username: 'JaneDoe', email: 'jane@example.com', phone: '987-654-3210', creationDate: '2022-02-01' },
-    
-//   ];
-
-//   res.json(userData);
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running at http://localhost:${port}`);
-// });
